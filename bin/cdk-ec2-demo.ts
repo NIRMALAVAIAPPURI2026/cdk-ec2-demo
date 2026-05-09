da0a1a479f55251f3e84616345ec88bb3b6c876d
NIRMALAVAIAPPURI2026 <nirmalabioprout@gmail.com>
2026-05-09 07:17:34 +0000

    set cdk region to us-east-1

--- bin/cdk-ec2-demo.ts
+++ bin/cdk-ec2-demo.ts
@@ -7,6 +7,6 @@ const app = new cdk.App();
 new CdkEc2DemoStack(app, 'CdkEc2DemoStack', {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
-    region: process.env.CDK_DEFAULT_REGION || 'ap-southeast-1',
+    region: process.env.CDK_DEFAULT_REGION || 'us-east-1',
   },
 });
